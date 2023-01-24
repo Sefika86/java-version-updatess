158c5e9f8b2cc031e20550317d45ed7538ce7806
sefika86 <sefikaakin86@gmail.com>
2023-01-24 07:30:41 -0500

    Generics

--- m1-generics/src/com/company/generics/Team.java
+++ m1-generics/src/com/company/generics/Team.java
@@ -3,13 +3,15 @@ package com.company.generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team <T> {  //If we write <T> only that means any class(String, Integer...) is accepted
-                         //,but <T extends Player> accepts
+public class Team <T extends Player> {  //If we write <T> only that means any class(String, Integer...) is accepted
+                                         //,but <T extends Player> accepts only players, (basically looking for Player type)
 
     private String name;
     private List<T> members = new ArrayList<T>();
-
-
+    //if there was here <Player> instead of T I was able to put all kind of players in the same team which is not normal
+    //,but when I apply Generics(with T)--> Team<SoccerPlayer> liverpool = new Team("Liverpool");
+    //I can create any team, not open to all kind of classes anymore because of <T extends Player>
+    //and if I write <T> beside Class Team above, it accepts all classes --> Team< String>.......which we do not want
     public Team(String name) {
         this.name = name;
     }
