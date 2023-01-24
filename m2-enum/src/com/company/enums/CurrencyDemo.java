2bcb6cc831583187c7453794f46e5d9aa7ad1793
sefika86 <sefikaakin86@gmail.com>
2023-01-24 12:37:12 -0500

    Enums

--- m2-enum/src/com/company/enums/CurrencyDemo.java
+++ m2-enum/src/com/company/enums/CurrencyDemo.java
@@ -10,5 +10,7 @@ public class CurrencyDemo {
     public static final CurrencyDemo QUARTER = new CurrencyDemo();
 
     //these are constant objects, how I know== they are final
+    // for example here I am able to create CurrencyDemo() objects by default cons
+    //but in an enum; we do not need to creat an object, those NICKEL, PENNY, MULTIPLY etc. are already objects
 
 }
