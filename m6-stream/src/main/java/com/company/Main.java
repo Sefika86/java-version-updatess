999f8efd0c204fcc5411217e5b9358160e9fb0dc
sefika86 <sefikaakin86@gmail.com>
2023-02-02 06:50:00 -0500

    Stream

---
+++ m6-stream/src/main/java/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //How to create a STREAM from Array
+
+        String[] courses = {"Java", "Sprint", "Agile"};
+
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        //How to create Stream from Collection
+        List<String> courseList = Arrays.asList("Java", "Sprint", "Agile");
+
+        Stream<String> courseStream2 = courseList.stream();
+
+        //----------------------------------------------------------------------------
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java", 100),
+                new Course("DS", 101),
+                new Course("Ms", 102)
+        );
+
+        //now we will concert this source to a Stream
+
+        Stream<Course> myCourseStream = myCourses.stream();
+    }
+}
