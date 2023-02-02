220231d6b45c45525ce075690b9c15ae9d9693d5
sefika86 <sefikaakin86@gmail.com>
2023-02-02 05:45:42 -0500

    Comparator Interface

--- m5-comparator-interface/src/main/java/com/company/AppleTest.java
+++ m5-comparator-interface/src/main/java/com/company/AppleTest.java
@@ -1,7 +1,10 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class AppleTest {
     public static void main(String[] args) {
@@ -13,6 +16,27 @@ public class AppleTest {
         inventory.add(new Apple(Color.RED, 100));
         inventory.add(new Apple(Color.GREEN, 200));
         inventory.add(new Apple(Color.RED, 50));
+
+        Comparator<Apple> sortApple = Comparator.comparing((Apple apple) -> apple.getWeight());
+        inventory.sort(sortApple);
+        System.out.println(inventory);
+
+        //now shorter codes in java
+
+                       //we imported static java.util.Comparator.comparing, instead of calling the method through the interface name
+        inventory.sort(comparing(Apple::getWeight));    //we did in ascending order here
+        System.out.println(inventory);
+
+        //there are to many ready functions in this
+        //reversed()
+        inventory.sort(comparing(Apple::getWeight).reversed());   // in ascending order by using reverse()
+        System.out.println(inventory);
+
+        //Chaining
+        inventory
+                .sort(comparing(Apple::getWeight)
+                .reversed().thenComparing(Apple::getColor));
+
 
     }
 }
